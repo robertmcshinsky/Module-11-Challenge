d6d4594512ff1dac78d21b6422bd77f38fef25a8
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-04-21 22:16:28 -0600

    Completed Post route

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const port = process.env.PORT || 3000;
 const { notes } = require("./data/db.json");
 
 const fs = require("fs");
+const path = require("path");
 ////////////////////////////////
 
 function filterByQuery(query, notesArray) {
+  console.log("@filterByQuery");
   let filtered = notesArray;
 
   if (query.id) {
@@ -25,15 +27,30 @@ function filterByQuery(query, notesArray) {
   return filtered;
 }
 
+function createNewNote(body, notesArray) {
+  console.log("@createNewNote");
+  console.log(body);
+  let note = body;
+  notesArray.push(note);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./data/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+
+  return note;
+}
 ////////////////////////////////
 
 //! GET HOME ROUTE
 app.get("/", function (req, res) {
+  console.log("@getHomeRoute");
   res.send("@ / get route");
 });
 
 //! GET API NOTES ROUTE
 app.get("/api/notes", function (req, res) {
+  console.log("@getAPINotesRoute");
   let results = notes;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -43,6 +60,11 @@ app.get("/api/notes", function (req, res) {
 
 //! POST API NOTES ROUTE
 app.post("/api/notes", function (req, res) {
+  console.log("@PostAPINotesRoute");
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+
   console.log(req.body);
   res.json(req.body);
 });
@@ -50,5 +72,6 @@ app.post("/api/notes", function (req, res) {
 ////////////////////////////////
 
 app.listen(port, function () {
+  console.log("@Listening");
   console.log("Listening on port " + port);
 });
